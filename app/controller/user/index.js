c5aff502e4216a8dd7c29caa1dee5e9a109ee824
柚子 <707107790@qq.com>
2020-04-09 18:01:13 +0800

    fixed

--- app/controller/user/index.js
+++ app/controller/user/index.js
@@ -1,32 +1,27 @@
 const User = require("../../model/user.model");
-const mongoose = require('mongoose')
-const BaseController = require('../BaseController')
+const BaseController = require("../BaseController");
 class UserController extends BaseController {
-    constructor() {
-        super()
-        this.getUserInfo = this.getUserInfo.bind(this)
-    }
-    async getUserInfo(ctx, next) {
-        let data = await User.find()
-        this.formatData({
-            ctx: ctx,
-            data: data
-        })
-    }
+  constructor() {
+    super();
+    this.getUserInfo = this.getUserInfo.bind(this);
+  }
+  async getUserInfo(ctx, next) {
+      let user = await User.create({ firstName: "John Doe", lastName: "senior engineer" })
+      this.formatData({ ctx: ctx, data: { user } })
+  }
 
-    async saveUser(ctx, next) {
-        await next()
-        let data = await new User({
-            _id: new mongoose.Types.ObjectId(),
-            username: ctx.request.query.username,
-            password: ctx.request.query.password,
-        }).save()
-        this.formatData({
-            ctx: ctx,
-            data: data
-        })
-    }
-
+  async saveUser(ctx, next) {
+    await next();
+    let data = await new User({
+      _id: new mongoose.Types.ObjectId(),
+      username: ctx.request.query.username,
+      password: ctx.request.query.password
+    }).save();
+    this.formatData({
+      ctx: ctx,
+      data: data
+    });
+  }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
--- app/model/user.model.js
+++ app/model/user.model.js
@@ -1,4 +1,31 @@
-var mongoose = require("mongoose");
-var UsersSchema = require('../schemas/user.schema')
-var User = mongoose.model("user", UsersSchema);
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = require("../../db/index");
+
+const User = sequelize.define(
+    "User",
+    {
+        id : {
+            type: Sequelize.STRING(50),
+            primaryKey : true,
+            autoIncrement : true,
+            unique: true
+        },
+        // 在这里定义模型属性
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: null,
+        },
+        lastName: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        freezeTableName: true, //强制表名称等于模型名称
+        tableName: 'User', //直接提供表名
+        timestamps: true , //时间戳
+    }
+);
+
+// User.sync();
 module.exports = User;
